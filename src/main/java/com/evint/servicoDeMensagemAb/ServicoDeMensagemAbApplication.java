50dcc88e898c0baadefbebe92dc67044ec99279b
Yuri Autran <ylautran@gmail.com>
2024-06-07 15:49:16 -0300

    Project created

---
+++ src/main/java/com/evint/servicoDeMensagemAb/ServicoDeMensagemAbApplication.java
@@ -0,0 +1,13 @@
+package com.evint.servicoDeMensagemAb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoDeMensagemAbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoDeMensagemAbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evint/servicoDeMensagemAb/ServicoDeMensagemAbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evint.servicoDeMensagemAb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoDeMensagemAbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
