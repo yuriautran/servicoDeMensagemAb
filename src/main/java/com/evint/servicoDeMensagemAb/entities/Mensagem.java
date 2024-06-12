6b81475f0e5398e6f88dde6b8f41f8922e5658c0
Yuri Autran <ylautran@gmail.com>
2024-06-12 15:45:52 -0300

    Entity Mensagem created

---
+++ src/main/java/com/evint/servicoDeMensagemAb/entities/Mensagem.java
@@ -0,0 +1,76 @@
+package com.evint.servicoDeMensagemAb.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "mensagem")
+public class Mensagem implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String titulo;
+	private String descricao;
+	
+	public Mensagem() {
+		
+	}
+
+	public Mensagem(Long id, String titulo, String descricao) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.descricao = descricao;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Mensagem other = (Mensagem) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
